07fe1ed332ce3c4e6532e610ff67ea6e34d763a8
AndressaSRodrigues <srandressa2@gmail.com>
2023-11-02 12:39:19 -0300

    Refactored auth/login controller and route

--- routes/auth.js
+++ routes/auth.js
@@ -1,33 +1,7 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const config = require('../config');
-const { getUserByEmail } = require('../controller/users');
-
-const { secret } = config;
+const { login } = require('../controller/auth');
 
 module.exports = (app, nextMain) => {
-  app.post('/login', async (req, res, next) => {
-    const { email, password } = req.body;
-
-    if (!email || !password) {
-      return next(400);
-    }
-
-    const user = await getUserByEmail(email);
-
-    if (!user) {
-      return res.status(400).json({ err: 'User not found' });
-    }
-
-    const validatePassword = bcrypt.compareSync(password, user.password);
-
-    if (!validatePassword) {
-      return res.status(400).json({ err: 'Invalid password' });
-    }
-
-    const token = jwt.sign({ id: user.id, email: user.email, role: user.role }, secret);
-    return res.json({ accessToken: token, user });
-  });
+  app.post('/login', login);
 
   return nextMain();
 };
